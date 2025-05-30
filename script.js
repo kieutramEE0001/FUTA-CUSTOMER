30cc7b642a1a958dfb7a228afb7ec863de5c668e
kieutramEE0001 <tramduong.31231022205@st.ueh.edu.vn>
2025-05-31 06:48:51 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Chức năng chuyển slide (nếu muốn phát triển thêm)
+document.querySelector(".arrow.left").addEventListener("click", () => {
+  alert("Chuyển slide trái");
+});
+document.querySelector(".arrow.right").addEventListener("click", () => {
+  alert("Chuyển slide phải");
+});
